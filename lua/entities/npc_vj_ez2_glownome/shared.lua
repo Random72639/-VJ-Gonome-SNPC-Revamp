c428be5611ca586fb1eadc8eaa852ea096e1a58f
Random72638 <antoniogurrerio444@gmail.com>
2024-09-30 22:16:31 +1000

    Initial commit

---
+++ lua/entities/npc_vj_ez2_glownome/shared.lua
@@ -0,0 +1,17 @@
+ENT.Base 			= "npc_vj_ez2_gonome"
+ENT.Type 			= "ai"
+ENT.PrintName 		= "Entropy Zero 2 Glownome"
+ENT.Author 			= "Random72638"
+ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
+ENT.Purpose 		= "Spawn it and fight with it!"
+ENT.Instructions 	= "Click on the spawnicon to spawn it."
+ENT.Category		= "Half-life"
+
+if (CLIENT) then
+local Name = "Entropy Zero 2 Glownome"
+local LangName = "npc_vj_ez2_glownome"
+language.Add(LangName, Name)
+killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
+language.Add("#"..LangName, Name)
+killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
+end
---
+++ lua/entities/obj_gonome_electric_bolt/shared.lua
@@ -0,0 +1,16 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "obj_vj_projectile_base"
+ENT.PrintName		= "Bruh"
+ENT.Author 			= "SOMEBODY SAY HO!"
+ENT.Contact 		= "http://steamcommunity.com/profiles/76561198180831682/"
+ENT.Information		= "Projectiles for my addons"
+ENT.Category		= "Projectiles"
+
+if (CLIENT) then
+	local Name = "Bruh"
+	local LangName = "obj_gonome_electric_bolt"
+	language.Add(LangName, Name)
+	killicon.Add(LangName,"HUD/killicons/default",Color(255,80,0,255))
+	language.Add("#"..LangName, Name)
+	killicon.Add("#"..LangName,"HUD/killicons/default",Color(255,80,0,255))
+end
